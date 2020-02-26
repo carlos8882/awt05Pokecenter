49f0806c194c981169e5cc8bf5c1a345fe4e48e0
ArielYanarico <Ariel.Yanarico@fundacion-jala.org>
2020-02-26 13:32:38 -0400

    This is the very beginning commit with some examples and structure

---
+++ src/main/java/org/awt05/Main.java
@@ -0,0 +1,14 @@
+package org.awt05;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = getHello();
+
+        System.out.println(hello);
+    }
+
+    public static String getHello() {
+        return "Hello I am in a poke center";
+    }
+
+}
