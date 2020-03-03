93f95f2bd800f5c0f8cfd143dd6df62fd9d4adb2
Diego <jdhpp_perez@hotmail.com>
2020-03-03 16:57:04 -0400

    Revert "adapting a method to standards"
    This reverts commit fbc71158085646381b3b9994cc879dfcbac07937.

--- src/main/java/org/awt05/skills/AbleToStrength.java
+++ src/main/java/org/awt05/skills/AbleToStrength.java
@@ -1,7 +1,7 @@
 package org.awt05.skills;
 
 public interface AbleToStrength {
-    default void strength() {
+    default void Strength() {
         System.out.println("... has used STRENGTH!!");
     }
 }
